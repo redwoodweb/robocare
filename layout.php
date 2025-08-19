396cc6ab75c93461f8ace603d04df42046e67400
rihno1 <redwoodweb@naver.com>
2025-08-19 20:38:48 +0900

    add navgation in main
    add navgation in main

--- layout.php
+++ layout.php
@@ -16,10 +16,15 @@ include 'header.php';
             <ul>
                 <li><a href="./sign_up.php">회원가입</a></li>
                 <li><a href="./user_info_view.php">사용자 정보관리 확인/수정</a></li>
-                <li><a href="./authentication.php">본인인증페이지</a></li>
+                <li><a href="./authentication.php">본인 인증 페이지</a></li>
+                <li><a href="./completed_sign_up.php">회원가입 완료</a></li>
+                <li><a href="./find_id.php">아이디 찾기</a></li>
+                <li><a href="./result_find_id.php">아이디 찾기 완료</a></li>
+                <li><a href="./agreement.php">약관동의</a></li>
+                <li><a href="./user_info_view_auth.php.php">사용자 정보관리 인증</a></li>
             </ul>
             <?php
-                //해당 컴포넌트 : sign_up.php(회원가입),user_info_view.php(사용자 정보관리 확인/수정),authentication.php(본인인증페이지),compeleted_sign_up.php(회원가입완료)
+                //해당 컴포넌트 : sign_up.php(회원가입),user_info_view.php(사용자 정보관리 확인/수정),authentication.php(본인인증페이지),completed_sign_up.php(회원가입완료)
                 /////////////// find_id.php(아이디 찾기),result_find_id.php(아이디 찾기 완료), agreement.php(약관동의), user_info_view_auth.php(사용자 정보관리 인증)                
             ?>
         </div>
---
+++ sign_up.php
@@ -0,0 +1,37 @@
+<?php
+$layout = 'common';
+include 'header.php';
+?>
+    <!-- 네비게이션 없을 경우 헤더 -->
+    <header>
+        <div class="logo">
+            <a href="/" alt="홈으로 가기"><img src="public/img/top_logo.png" alt="GST 로고 이미지"></a>
+        </div>
+    </header>
+    <!-- //.네비게이션 없을 경우 헤더 -->
+    <!-- content : 콘텐츠 영역 -->
+    <div class="content">
+        <!-- content_inner -->
+        <div class="content_inner">
+            <?php
+                //해당 컴포넌트 : sign_up.php(회원가입),user_info_view.php(사용자 정보관리 확인/수정),authentication.php(본인인증페이지),compeleted_sign_up.php(회원가입완료)
+                /////////////// find_id.php(아이디 찾기),result_find_id.php(아이디 찾기 완료), agreement.php(약관동의), user_info_view_auth.php(사용자 정보관리 인증)
+                include 'components/sign_up.php';
+                // include 'components/user_info_view.php';
+                //include 'components/authentication.php';
+                //include 'components/compeleted_sign_up.php';
+                //include 'components/find_id.php';
+                //include 'components/result_find_id.php';
+                //include 'components/agreement.php';
+                //include 'components/user_info_view_auth.php';
+                //include 'components/user_info_view_auth.php';
+            ?>
+        </div>
+        <!-- //.content_inner -->
+    </div>
+    <!-- //.content -->
+
+<?php
+$fLayout = 'f_relative';
+include 'footer.php';
+?>
