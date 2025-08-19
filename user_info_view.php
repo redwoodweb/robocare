c008c2e68f9e35a7cb831b5463d488f25d406f18
rihno1 <redwoodweb@naver.com>
2025-08-19 20:20:07 +0900

    페이지 분리
    회우언가입, 사용자 정보관리 확인/수정
    본인인증페이지

--- user_info_view.php
+++ user_info_view.php
@@ -13,12 +13,18 @@ include 'header.php';
     <div class="content">
         <!-- content_inner -->
         <div class="content_inner">
-            <ul>
-                <li><a href="./sign_up.php">회원가입</a></li>
-            </ul>
             <?php
                 //해당 컴포넌트 : sign_up.php(회원가입),user_info_view.php(사용자 정보관리 확인/수정),authentication.php(본인인증페이지),compeleted_sign_up.php(회원가입완료)
-                /////////////// find_id.php(아이디 찾기),result_find_id.php(아이디 찾기 완료), agreement.php(약관동의), user_info_view_auth.php(사용자 정보관리 인증)                
+                /////////////// find_id.php(아이디 찾기),result_find_id.php(아이디 찾기 완료), agreement.php(약관동의), user_info_view_auth.php(사용자 정보관리 인증)
+                // include 'components/sign_up.php';
+                include 'components/user_info_view.php';
+                //include 'components/authentication.php';
+                //include 'components/compeleted_sign_up.php';
+                //include 'components/find_id.php';
+                //include 'components/result_find_id.php';
+                //include 'components/agreement.php';
+                //include 'components/user_info_view_auth.php';
+                //include 'components/user_info_view_auth.php';
             ?>
         </div>
         <!-- //.content_inner -->
