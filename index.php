5dad2e6ad1a0d239868b4e6496c01dcecb12be56
rihno1 <redwoodweb@naver.com>
2025-08-20 15:45:48 +0900

    sass option set
    sass option set

--- index.php
+++ index.php
@@ -17,10 +17,8 @@ include 'header.php';
                 <li><a href="./agreement.php">약관동의</a></li>
                 <li><a href="./user_info_view_auth.php">사용자 정보관리 인증</a></li>
             </ul>
-            <?php
-                //해당 컴포넌트 : sign_up.php(회원가입),user_info_view.php(사용자 정보관리 확인/수정),authentication.php(본인인증페이지),completed_sign_up.php(회원가입완료)
-                /////////////// find_id.php(아이디 찾기),result_find_id.php(아이디 찾기 완료), agreement.php(약관동의), user_info_view_auth.php(사용자 정보관리 인증)                
-            ?>
+            //해당 컴포넌트 : sign_up.php(회원가입),user_info_view.php(사용자 정보관리 확인/수정),authentication.php(본인인증페이지),completed_sign_up.php(회원가입완료)
+            /////////////// find_id.php(아이디 찾기),result_find_id.php(아이디 찾기 완료), agreement.php(약관동의), user_info_view_auth.php(사용자 정보관리 인증)            
         </div>
         <!-- //.content_inner -->
     </div>
